44e0e69fae24942ef112029bebfc87dee1f5fa6d
AnNuX0 <42578843+AnNuX0@users.noreply.github.com>
2023-08-24 10:11:43 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@ import streamlit
 streamlit.title('Hello! World, StreamLet App here!')
 
 streamlit.header('Header Text')
-
 streamlit.text('Text1!')
 streamlit.text('Text2!')
 streamlit.text('Text3!')
