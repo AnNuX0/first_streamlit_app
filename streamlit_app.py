89797a5dffe05ad47e11b1397b91a45e32c57680
AnNuX0 <42578843+AnNuX0@users.noreply.github.com>
2023-08-24 11:35:41 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('Hello! World, StreamLet App here!')
 
@@ -11,3 +12,5 @@ streamlit.text('Text3!')
 streamlit.text('Text4!')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
