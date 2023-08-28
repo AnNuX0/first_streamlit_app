e3f9ee9b1ae0fe93fddc3c8f1511fa80d5ea94b4
AnNuX0 <42578843+AnNuX0@users.noreply.github.com>
2023-08-28 13:19:10 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,6 @@ import pandas
 streamlit.title('Hello! World, StreamLet App here!')
 
 streamlit.header('Header Text') 
-
 streamlit.text(' 🥣 🥗 🐔 🥑 🍞')
 
 streamlit.text('Text2!')
@@ -14,7 +13,6 @@ streamlit.text('Text4!')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
